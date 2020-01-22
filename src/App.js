47ddfc716b0b2facdc83d58246d208e7f36ef804
Hieu Nguyen <hieunm.citynow@gmail.com>
2020-01-22 14:37:52 +0700

    Changed the location of styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { LogInPage, HomePage } from "pages";
 import { configureDataStore } from "store/dataStore";
 import { useStore } from "store";
-import './common/styles/style.scss'
+import 'styles/style.scss'
 
 configureDataStore();
 
