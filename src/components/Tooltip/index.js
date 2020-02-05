f2f1db10be0126539f91143ccbfbf4f35643ee81
Hieu Nguyen <hieunm.citynow@gmail.com>
2020-02-05 22:07:32 +0700

    Added Tooltip

---
+++ src/components/Tooltip/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import styles from "./Tooltip.module.scss";
+
+const Tooltip = props => {
+  const { className, children, placement,...others } = props;
+
+  const classes = classNames({
+    [styles["mono__tooltip"]]: true,
+    [className]: className,
+    [styles[`mono__tooltip--${placement}`]]: placement
+  });
+
+  return <div className={classes} {...others} >
+      {children}
+    </div>;
+};
+
+Tooltip.defaultProps = {
+    placement: 'top'
+}
+
+Tooltip.propTypes = {
+    placement: PropTypes.oneOf(['top', 'left', 'right', 'bottom']),
+    title: PropTypes.string.isRequired
+}
+export default Tooltip;
--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import RadioBox from "./RadioBox";
 import RadioList from "./RadioList";
 import Table from "./Table";
 import Tag from "./Tag";
+import Tooltip from "./Tooltip";
 import Uploader from "./Uploader";
 
 export {
@@ -33,5 +34,6 @@ export {
   RadioList,
   Table,
   Tag,
+  Tooltip,
   Uploader,
 };
