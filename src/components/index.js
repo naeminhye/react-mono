a2fcbb4a653a94010feb65f6378c543d17aa30c6
Hieu Nguyen <hieunm.citynow@gmail.com>
2020-05-06 23:22:19 +0700

    Added Collapse

--- src/components/index.js
+++ src/components/index.js
@@ -1,28 +1,29 @@
-import Avatar from './Avatar';
-import Button from './Button';
-import Card from './Card';
-import Carousel from './Carousel';
-import CheckBox from './CheckBox';
-import CheckList from './CheckList';
-import DatePicker from './DatePicker';
-import FlipCard from './FlipCard';
-import DropDown from './DropDown';
-import Icons from './Icons';
-import Input from './Input';
-import Fabric from './Fabric';
-import Form from './Form';
-import List from './List';
-import Overlay from './Overlay';
-import Pagination from './Pagination';
-import Progress from './Progress';
-import RadioBox from './RadioBox';
-import RadioList from './RadioList';
-import Steps from './Steps';
-import Table from './Table';
-import Tag from './Tag';
-import ToggleSwitch from './ToggleSwitch';
-import Tooltip from './Tooltip';
-import Uploader from './Uploader';
+import Avatar from "./Avatar";
+import Button from "./Button";
+import Card from "./Card";
+import Carousel from "./Carousel";
+import CheckBox from "./CheckBox";
+import CheckList from "./CheckList";
+import Collapse from "./Collapse";
+import DatePicker from "./DatePicker";
+import FlipCard from "./FlipCard";
+import DropDown from "./DropDown";
+import Icons from "./Icons";
+import Input from "./Input";
+import Fabric from "./Fabric";
+import Form from "./Form";
+import List from "./List";
+import Overlay from "./Overlay";
+import Pagination from "./Pagination";
+import Progress from "./Progress";
+import RadioBox from "./RadioBox";
+import RadioList from "./RadioList";
+import Steps from "./Steps";
+import Table from "./Table";
+import Tag from "./Tag";
+import ToggleSwitch from "./ToggleSwitch";
+import Tooltip from "./Tooltip";
+import Uploader from "./Uploader";
 
 export {
   Avatar,
@@ -31,6 +32,7 @@ export {
   Carousel,
   CheckBox,
   CheckList,
+  Collapse,
   DatePicker,
   DropDown,
   Input,
