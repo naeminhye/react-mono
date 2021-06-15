d12859a676f08aff35d82b5c3ce131fc597925e5
Jacey Nae <minhhieu.eng2@gmail.com>
2021-06-16 00:14:55 +0700

    [RMN-15] Update table component (#39)
    * [RMN-15] Update Table component
    
    * Add Tree example

--- src/App.js
+++ src/App.js
@@ -1,95 +1,12 @@
-import React from "react";
-import { Tree } from "components";
-
-// Sample Tree dataSource
-const dataSource = [
-  {
-    value: "1",
-    title: "Tree Item 1",
-    children: [
-      {
-        value: "1-1",
-        title: "Tree Item 1-1",
-      },
-      {
-        value: "1-2",
-        title: "Tree Item 1-2",
-        children: [
-          {
-            value: "1-2-1",
-            title: "Tree Item 1-2-1",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: "2",
-    title: "Tree Item 2",
-    children: [
-      {
-        value: "2-1",
-        title: "Tree Item 2-1",
-      },
-      {
-        value: "2-2",
-        title: "Tree Item 2-2",
-      },
-    ],
-  },
-  {
-    value: "3",
-    title: "Tree Item 3",
-  },
-  {
-    value: "4",
-    title: "Tree Item 4",
-  },
-  {
-    value: "5",
-    title: "Tree Item 5",
-  },
-];
-const x = 3;
-const y = 2;
-const z = 1;
-const gData = [];
-
-const generateData = (_level, _preKey, _tns) => {
-  const preKey = _preKey || '0';
-  const tns = _tns || gData;
-
-  const children = [];
-  for (let i = 0; i < x; i++) {
-    const value = `${preKey}-${i}`;
-    tns.push({ title: value, value });
-    if (i < y) {
-      children.push(value);
-    }
-  }
-  if (_level < 0) {
-    return tns;
-  }
-  const level = _level - 1;
-  children.forEach((key, index) => {
-    tns[index].children = [];
-    return generateData(level, key, tns[index].children);
-  });
-};
-generateData(z);
-
+import React from 'react';
+import TreeExample from 'example/Tree';
+import TableExample from 'example/Table';
 
 const App = () => {
   return (
     <div>
-      <Tree
-        dataSource={gData}
-        defaultExpandedKeys={[]}
-        defaultSelectedKeys={[]}
-        // selectable
-        // multiple
-        // checkable
-      />
+      {/* <TreeExample /> */}
+      <TableExample />
     </div>
   );
 };
---
+++ src/example/Table/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Table, Tag, Button } from 'components';
+
+import dataSource from './mock';
+
+const columns = [
+  {
+    title: 'Name',
+    key: 'name',
+    dataIndex: 'name',
+    sortable: true,
+  },
+  {
+    title: 'Gender',
+    key: 'gender',
+    dataIndex: 'gender',
+    render: (value) => (value ? <Tag>Male</Tag> : <Tag>Female</Tag>),
+  },
+  {
+    title: 'Phone',
+    key: 'phone',
+    dataIndex: 'phone',
+  },
+  {
+    title: 'Address',
+    key: 'address',
+    dataIndex: 'address',
+  },
+  {
+    title: 'Email',
+    key: 'email',
+    dataIndex: 'email',
+  },
+  {
+    title: 'Actions',
+    key: 'actions',
+    render: () => {
+      return (
+        <div>
+          <Button
+            size="xs"
+            style={{ minWidth: 80, marginRight: 16 }}
+            type="primary"
+          >
+            Edit
+          </Button>
+          <Button size="xs" style={{ minWidth: 80 }} type="danger">
+            Delete
+          </Button>
+        </div>
+      );
+    },
+  },
+];
+
+const TableExample = () => {
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        bordered
+        loading={false}
+        striped
+        rowSelection={{
+          onChange: (selectedRowKeys) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`);
+          },
+          onSelect: (record, selected, selectedRows) => {
+            console.log('onSelect', record, selected, selectedRows);
+          },
+          onSelectAll: (selected, selectedRows) => {
+            console.log('onSelectAll', selected, selectedRows);
+          },
+        }}
+        pagination={{
+          current: 1,
+          pageSize: 5,
+          total: dataSource.length,
+          onChange: (page) => {
+            // console.log('onChange to', page);
+          },
+          onShowSizeChange: () => {},
+        }}
+      />
+    </div>
+  );
+};
+
+export default TableExample;
