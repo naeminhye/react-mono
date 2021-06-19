ca7a0017c699400bda5068ebd446f9cff780420a
Hieu Nguyen <hieunm.citynow@gmail.com>
2021-06-19 15:09:35 +0700

    Updates

---
+++ precache-manifest.83cd8bcca8e2eead8fd70bce9447de95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8783a629e0f8eb890ce8486c7c2da2",
+    "url": "/react-mono-storybook/index.html"
+  },
+  {
+    "revision": "b2507447aacc14caca94",
+    "url": "/react-mono-storybook/static/css/main.3a1a36f1.chunk.css"
+  },
+  {
+    "revision": "56867cca6f0973d114dc",
+    "url": "/react-mono-storybook/static/js/2.f53427bd.chunk.js"
+  },
+  {
+    "revision": "ca46ca3c9f35601fd99ef30fb6cf9aa6",
+    "url": "/react-mono-storybook/static/js/2.f53427bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2507447aacc14caca94",
+    "url": "/react-mono-storybook/static/js/main.63d63489.chunk.js"
+  },
+  {
+    "revision": "4b00994e893e537e9b91",
+    "url": "/react-mono-storybook/static/js/runtime-main.a058d739.js"
+  }
+]);
