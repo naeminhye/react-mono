b3d655fa4389d55b2a6a647d1d8916535540436e
Jacey Nae <minhhieu.eng2@gmail.com>
2021-06-19 23:43:28 +0700

    [RMN-29] Add Cursor Decorator Component (#41)
    Co-authored-by: Hieu Nguyen <hieunm.citynow@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import TreeExample from 'example/Tree';
-import TableExample from 'example/Table';
+// import TreeExample from 'example/Tree';
+// import TableExample from 'example/Table';
+import CursorExample from 'example/Cursor';
 
 const App = () => {
   return (
     <div>
-      {/* <TreeExample /> */}
-      <TableExample />
+      <CursorExample />
     </div>
   );
 };
